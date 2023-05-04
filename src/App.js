82d55bcba9b72cc22e3bdcf15eeb80a094bd4b55
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-05-04 15:11:52 -0700

    WIP with useContext

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import { UserContext } from "./lib/UserContext";
+import Callback from "./pages/Callback";
+
 function App() {
-  return <div>Discord</div>;
+  const [user, setUser] = useState();
+
+  return (
+    <UserContext.Provider value={[user, setUser]}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          {/* <Route path="/callback" element={<Callback />} /> */}
+        </Routes>
+      </Router>
+    </UserContext.Provider>
+  );
 }
 
 export default App;
