bbc2bbee592626d184fb7b7cc8ae2df9664a7d81
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2023-05-04 16:01:37 -0700

    adds user info displays

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import { magic } from "../lib/magic";
 import { useNavigate } from "react-router-dom";
+import MagicInfo from "../components/MagicInfo";
+import OAuthInfo from "../components/OAuthInfo";
 
 const Dashboard = () => {
   const [user, setUser] = useState();
@@ -9,7 +11,6 @@ const Dashboard = () => {
   const finishSocialLogin = async () => {
     const result = await magic.oauth.getRedirectResult();
     setUser(result);
-    console.log(result);
   };
 
   useEffect(() => {
@@ -25,7 +26,13 @@ const Dashboard = () => {
     <div>
       <h1>Dashboard</h1>
       {!user && <div>Loading...</div>}
-      {user && <div>{}</div>}
+
+      {user && (
+        <div>
+          <MagicInfo data={user.magic.userMetadata} />
+          <OAuthInfo data={user.oauth} />
+        </div>
+      )}
       <button onClick={logout}>Logout</button>
     </div>
   );
